73bf319f115ca217e131f13a624e5bee0611625b
cpjudgesarchive <85777003+cpjudgesarchive@users.noreply.github.com>
2021-10-08 21:53:53 +0530

    svg

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -36,7 +36,13 @@ const IconLoader = () => (
         strokeWidth="5"
         strokeLinecap="round"
         strokeLinejoin="round"
-        d="M40 1L1 23V68L40 91L79 69V24L40 1Z"
+        // d="M40 1L1 23V68L40 91L79 69V24L40 1Z"
+        d="M 50, 5
+          L 11, 27
+          L 11, 72
+          L 50, 95
+          L 89, 73
+          L 89, 28 z"
       />
     </g>
   </svg>
