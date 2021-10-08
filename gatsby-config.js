c2995cbce6126fd9c3e674bb8e1dd8e70c0ac00b
cpjudgesarchive <85777003+cpjudgesarchive@users.noreply.github.com>
2021-10-08 20:11:10 +0530

    config-explored

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Sunil Shukla',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Sunil is a open-source contributer and who love to build websites for Small and Medium Businesses in India. He is specializing in building (and occasionally designing) exceptional digital experiences on web.',
+    siteUrl: 'https://sunil.pages.dev', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'SunilShukla',
+        short_name: 'SunilShukla',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
